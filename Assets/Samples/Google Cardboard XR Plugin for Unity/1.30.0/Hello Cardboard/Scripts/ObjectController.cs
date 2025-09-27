accba3988964fdd44c56444571c69ee379337d54
darck-link <203453365+Geas-lh@users.noreply.github.com>
2025-09-26 19:41:29 -0500

    1.7

--- Assets/Samples/Google Cardboard XR Plugin for Unity/1.30.0/Hello Cardboard/Scripts/ObjectController.cs
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.30.0/Hello Cardboard/Scripts/ObjectController.cs
@@ -20,23 +20,13 @@ using System.Collections;
 using UnityEngine;
 
 /// <summary>
-/// Controls target objects behaviour.
+/// Controls target object's behavior for gaze-based interaction.
 /// </summary>
 public class ObjectController : MonoBehaviour
 {
-    /// <summary>
-    /// The material to use when this object is inactive (not being gazed at).
-    /// </summary>
     public Material InactiveMaterial;
-
-    /// <summary>
-    /// The material to use when this object is active (gazed at).
-    /// </summary>
     public Material GazedAtMaterial;
 
-    // The objects are about 1 meter in radius, so the min/max target distance are
-    // set so that the objects are always within the room (which is about 5 meters
-    // across).
     private const float _minObjectDistance = 2.5f;
     private const float _maxObjectDistance = 3.5f;
     private const float _minObjectHeight = 0.5f;
@@ -45,10 +35,7 @@ public class ObjectController : MonoBehaviour
     private Renderer _myRenderer;
     private Vector3 _startingPosition;
 
-    /// <summary>
-    /// Start is called before the first frame update.
-    /// </summary>
-    public void Start()
+    void Start()
     {
         _startingPosition = transform.parent.localPosition;
         _myRenderer = GetComponent<Renderer>();
@@ -56,24 +43,45 @@ public class ObjectController : MonoBehaviour
     }
 
     /// <summary>
-    /// Teleports this instance randomly when triggered by a pointer click.
+    /// Called when gaze enters this object.
+    /// </summary>
+    public void OnPointerEnterXR()
+    {
+        SetMaterial(true);
+    }
+
+    /// <summary>
+    /// Called when gaze exits this object.
+    /// </summary>
+    public void OnPointerExitXR()
+    {
+        SetMaterial(false);
+    }
+
+    /// <summary>
+    /// Called when object is clicked while being gazed at.
+    /// </summary>
+    public void OnPointerClickXR()
+    {
+        TeleportRandomly();
+    }
+
+    /// <summary>
+    /// Teleports the object randomly within predefined bounds.
     /// </summary>
     public void TeleportRandomly()
     {
-        // Picks a random sibling, activates it and deactivates itself.
         int sibIdx = transform.GetSiblingIndex();
         int numSibs = transform.parent.childCount;
         sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
         GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
 
-        // Computes new object's location.
         float angle = Random.Range(-Mathf.PI, Mathf.PI);
         float distance = Random.Range(_minObjectDistance, _maxObjectDistance);
         float height = Random.Range(_minObjectHeight, _maxObjectHeight);
         Vector3 newPos = new Vector3(Mathf.Cos(angle) * distance, height,
                                      Mathf.Sin(angle) * distance);
 
-        // Moves the parent to the new position (siblings relative distance from their parent is 0).
         transform.parent.localPosition = newPos;
 
         randomSib.SetActive(true);
@@ -82,40 +90,11 @@ public class ObjectController : MonoBehaviour
     }
 
     /// <summary>
-    /// This method is called by the Main Camera when it starts gazing at this GameObject.
+    /// Changes material based on gaze status.
     /// </summary>
-    public void OnPointerEnter()
-    {
-        SetMaterial(true);
-    }
-
-    /// <summary>
-    /// This method is called by the Main Camera when it stops gazing at this GameObject.
-    /// </summary>
-    public void OnPointerExit()
-    {
-        SetMaterial(false);
-    }
-
-    /// <summary>
-    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
-    /// is touched.
-    /// </summary>
-    public void OnPointerClick()
-    {
-        TeleportRandomly();
-    }
-
-    /// <summary>
-    /// Sets this instance's material according to gazedAt status.
-    /// </summary>
-    ///
-    /// <param name="gazedAt">
-    /// Value `true` if this object is being gazed at, `false` otherwise.
-    /// </param>
     private void SetMaterial(bool gazedAt)
     {
-        if (InactiveMaterial != null && GazedAtMaterial != null)
+        if (_myRenderer != null && InactiveMaterial != null && GazedAtMaterial != null)
         {
             _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
         }
